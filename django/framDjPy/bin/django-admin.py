a7defa8757f171c5515652245acd50ab8dc96d71
khanilaunggu <kanilaunggu@Kanis-MacBook-Pro.local>
2020-01-12 14:11:44 +0700

    all files

---
+++ django/framDjPy/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kanilaunggu/Documents/DocPython/python-modules/django/framDjPy/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
